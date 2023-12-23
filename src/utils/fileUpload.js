04aebbf8827a10788dd804d3eddd7dc85f80edae
Saket Jain <saketj21@iitk.ac.in>
2023-12-23 12:42:10 +0530

    file upload

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadCloudinary = async (file) => {
+    try{
+      if(!file) return null;
+      const result = await cloudinary.uploader.upload(file,{
+            resource_type:"auto",
+        });
+        console.log("file uploaded succesfully",result);
+        return result;
+    }
+    catch(error){
+        fs.unlinkSync(file);
+        return null;
+    }
+}
+
+export {uploadCloudinary}
